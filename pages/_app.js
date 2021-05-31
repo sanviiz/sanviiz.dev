c0617c508a9f28209c493ee38864d53c3195b3ee
test <sanviiz06@hotmail.com>
2021-06-01 00:30:01 +0700

    Deleted package - React particles

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,4 @@
 import "../styles/globals.css";
-import Particles from "react-particles-js";
-import particles from "../assets/config/particles.json";
 import Navbar from "../components/navbar";
 import Footer from "../components/footer";
 import { AppContextWrapper } from "../contexts/state";
@@ -9,7 +7,6 @@ function MyApp({ Component, pageProps }) {
   return (
     <div className="font-sans">
       <AppContextWrapper>
-        <Particles params={particles} />
         <Navbar />
         <Component {...pageProps} />
         <Footer />
