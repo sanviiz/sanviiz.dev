7bd487882361abae714c5b734795559e209adfbb
test <sanviiz06@hotmail.com>
2021-06-01 21:03:01 +0700

    Update seo content - Description and keywords

--- assets/seo.js
+++ assets/seo.js
@@ -1,14 +1,14 @@
 export const HOME_PAGE_TITLE = `Home | `;
-export const HOME_DESCRIPTION_CONTENT = `Hi, I'm Pakkaphong Akkabut A front end developer and interested in the full-stack developer. - Sanviiz`;
-export const HOME_KEYWORDS_CONTENT = `Sanviiz, sanviiz, sanviiz.dev, sanviiz dev, sanviiz dev home, sanviiz home, index, sanviiz pakkaphong, Pakkaphong, Pakkaphong Akkabut, frontend, frontend developer, frontend dev, full-stack, full stack, dev`;
-export const HOME_OGDESCRIPTION_CONTENT = `Hi, I'm Pakkaphong Akkabut A front end developer and interested in the full-stack developer. - Sanviiz`;
+export const HOME_DESCRIPTION_CONTENT = `Hi, I'm Pakkaphong Akkabut A front end developer and interested in the full stack developer. - Sanviiz`;
+export const HOME_KEYWORDS_CONTENT = `Sanviiz, sanviiz, sanviiz.dev, sanviiz dev, sanviiz dev home, sanviiz home, index, sanviiz pakkaphong, Pakkaphong, Pakkaphong Akkabut, front end, frontend, front-end, frontend developer, frontend dev, full-stack, full stack, dev`;
+export const HOME_OGDESCRIPTION_CONTENT = `Hi, I'm Pakkaphong Akkabut A front end developer and interested in the full stack developer. - Sanviiz`;
 
 export const ABOUT_PAGE_TITLE = `About | `;
 export const ABOUT_DESCRIPTION_CONTENT = `Pakkaphong Akkabut - Present, Studying at King Mongkut’s University of Technology Thonbury 3rd Year Student Bachelor of Engineering in Electronic and Telecommunication Engineering.`;
-export const ABOUT_KEYWORDS_CONTENT = `Sanviiz, sanviiz, about sanviiz dev, all about sanviiz, about sanviiz, about, sanviiz pakkaphong, About Pakkaphong, About Pakkaphong Akkabut, frontend, frontend developer, frontend dev, full-stack, full stack, dev`;
+export const ABOUT_KEYWORDS_CONTENT = `Sanviiz, sanviiz, about sanviiz dev, all about sanviiz, about sanviiz, about, sanviiz pakkaphong, About Pakkaphong, About Pakkaphong Akkabut, front end, frontend, front-end, frontend developer, frontend dev, full-stack, full stack, dev`;
 export const ABOUT_OGDESCRIPTION_CONTENT = `Pakkaphong Akkabut - Present, Studying at King Mongkut’s University of Technology Thonbury 3rd Year Student Bachelor of Engineering in Electronic and Telecommunication Engineering.`;
 
 export const CONTACT_PAGE_TITLE = `Contact | `;
 export const CONTACT_DESCRIPTION_CONTENT = `Contact me - Pakkaphong Akkabut`;
-export const CONTACT_KEYWORDS_CONTENT = `Sanviiz, sanviiz, contact sanviiz dev, contact sanviiz, contact, sanviiz pakkaphong, Contact Pakkaphong, Contact Pakkaphong Akkabut, frontend, frontend developer, frontend dev, full-stack, full stack, dev`;
+export const CONTACT_KEYWORDS_CONTENT = `Sanviiz, sanviiz, contact sanviiz dev, contact sanviiz, contact, sanviiz pakkaphong, Contact Pakkaphong, Contact Pakkaphong Akkabut, front end, frontend, front-end, frontend developer, frontend dev, full-stack, full stack, dev`;
 export const CONTACT_OGDESCRIPTION_CONTENT = `Contact me - Pakkaphong Akkabut`;
