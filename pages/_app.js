18b5fed49bf6a007e891011128181c436061c0f1
test <sanviiz06@hotmail.com>
2021-06-02 18:00:05 +0700

    Adding google analytics lib

--- pages/_app.js
+++ pages/_app.js
@@ -1,9 +1,28 @@
 import "../styles/globals.css";
 import Navbar from "../components/navbar";
+import { useRouter } from "next/router";
 import Footer from "../components/footer";
 import { AppContextWrapper } from "../contexts/state";
+import * as ga from "../lib/ga";
 
 function MyApp({ Component, pageProps }) {
+  const router = useRouter();
+
+  useEffect(() => {
+    const handleRouteChange = (url) => {
+      ga.pageview(url);
+    };
+    //When the component is mounted, subscribe to router changes
+    //and log those page views
+    router.events.on("routeChangeComplete", handleRouteChange);
+
+    // If the component is unmounted, unsubscribe
+    // from the event with the `off` method
+    return () => {
+      router.events.off("routeChangeComplete", handleRouteChange);
+    };
+  }, [router.events]);
+
   return (
     <AppContextWrapper>
       <Navbar />
